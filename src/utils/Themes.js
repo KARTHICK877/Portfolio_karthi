76d791494ed6f15f24a6c534e6d9bbe18959f0ca
KARTHICK877 <kmass8754@gmail.com>
2024-01-01 19:18:25 +0530

    up

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,22 +1,24 @@
 export const darkTheme = {
-    bg:"#1C1C27",
-    bgLight: "#1C1E27",
-    primary:"#854CE6",
-    text_primary:"#F2F3F4",
-    text_secondary:"#b1b2b3",
-    card:"#171721",
-    card_light: '#191924',
-    button:"#854CE6",
-    white:"#FFFFFF",
-    black:"#000000",
-}
+    bg: "white",
+    bgLight: "#FF69B4",
+    primary: "#FF69B4",
+    text_primary: "#FF69B4",
+    text_secondary: "white",
+    card: "white",
+    card_light: "#191924",
+    button: "#854CE6",
+    white: "#FFFFFF",
+    black: "#000000",
+    pink: "#FF69B4", // Pink color added
+};
 
 export const lightTheme = {
-    bg:"#FFFFFF",
-    bgLight: "#f0f0f0",
-    primary:"#be1adb",
-    text_primary:"#111111",
-    text_secondary:"#48494a",
-    card:"#FFFFFF",
-    button:"#5c5b5b",
-}
+    bg: "pink",
+    bgLight: "pink",
+    primary: "#be1adb",
+    text_primary: "#111111",
+    text_secondary: "#48494a",
+    card: "#FFFFFF",
+    button: "#5c5b5b",
+    pink: "#FF69B4", // Pink color added
+};
