decd785d9be06b94f7e88ed3993dfdb667670972
KARTHICK877 <kmass8754@gmail.com>
2024-05-11 08:23:18 +0530

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/KARTHICK877",
   resume:
-    "https://drive.google.com/file/d/16N6vaIyBDAFiOCMrJpG7A0W91AQ8Q6Ya/view?usp=sharing",
+    "https://drive.google.com/file/d/1QV-9nelOj_LkRVoJRtvjAPT4HeUbYFAB/view?usp=sharing ",
   linkedin: "https://www.linkedin.com/in/karthick-k-r-04547028a/",
   twitter: "https://twitter.com/",
   insta: "https://www.instagram.com/kr_pulse200/?hl=en",
